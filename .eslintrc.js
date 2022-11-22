380ebe9c737c2912b72783de8c983bfddb952fdc
Umut KENAR <umut.kenar@hotmail.com>
2022-11-22 19:56:15 +0300

    refactor: some rules added for eslint and tailwind init

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,32 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/airbnb',
-  ],
+  extends: ['plugin:vue/vue3-essential', '@vue/airbnb'],
   parserOptions: {
     parser: '@babel/eslint-parser',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'global-require': 0,
+    'vue/no-unused-components': [
+      'error',
+      {
+        ignoreWhenBindingPresent: true,
+      },
+    ],
+    'vue/no-unused-vars': [
+      'error',
+      {
+        ignorePattern: '^_',
+      },
+    ],
+    'vue/multi-word-component-names': [
+      'error',
+      {
+        ignores: ['default'],
+      },
+    ],
+    'max-len': ['error', { code: 600 }],
   },
 };
