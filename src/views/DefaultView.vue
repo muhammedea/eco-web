eebeb844a41b173fa05dcc5f6bf9c1a94691b77a
Umut KENAR <umut.kenar@hotmail.com>
2022-11-30 17:44:02 +0300

    feat: page transition added

--- src/views/DefaultView.vue
+++ src/views/DefaultView.vue
@@ -1,6 +1,12 @@
 <template>
   <EcolariumHeader></EcolariumHeader>
-  <RouterView></RouterView>
+  <div class="max-w-[1440px] mx-auto">
+    <router-view v-slot="{ Component }">
+      <transition leave-to-class="opacity-0" leave-active-class="transition duration-300">
+        <Component :is="Component"></Component>
+      </transition>
+    </router-view>
+  </div>
 </template>
 <script setup>
 import EcolariumHeader from '@/components/basecomponents/EcolariumHeader.vue';
