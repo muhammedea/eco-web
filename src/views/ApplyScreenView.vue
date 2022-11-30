be595f8aba9646b17ea337ccccfab01e422899a5
Umut KENAR <umut.kenar@hotmail.com>
2022-11-30 17:42:07 +0300

    feat: apply page

--- src/views/ApplyScreenView.vue
+++ src/views/ApplyScreenView.vue
@@ -1,3 +1,36 @@
 <template>
-    <h1>Apply Screen</h1>
+    <div class="w-full min-h-full_screen flex justify-center items-center py-10 px-8">
+        <div class="flex flex-col gap-6 items-center justify-center">
+            <h1 class="text-3xl font-bold text-Black-and-White-Black">Apply</h1>
+            <div class="flex gap-10 flex-wrap justify-center">
+                <div class="flex flex-col gap-4">
+                    <div class="p-14 border-2 rounded-lg">
+                        <img src="@/assets/images/apply-trade.svg" alt="apply-trade">
+                    </div>
+                    <div class="flex flex-col text-center gap-1">
+                        <span class="text-2xl text-Black-and-White-Black font-semibold">Apply Trade</span>
+                        <span class="text-sm text-Grayscale-Grey-1">Lorem ipsum dolor sit amet</span>
+                    </div>
+                </div>
+                <div class="flex flex-col gap-4">
+                    <div class="p-14 border-2 rounded-lg">
+                        <img src="@/assets/images/apply-ecoproject.svg" alt="apply-ecoproject">
+                    </div>
+                    <div class="flex flex-col text-center gap-1">
+                        <span class="text-2xl text-Black-and-White-Black font-semibold">Apply EcoProject</span>
+                        <span class="text-sm text-Grayscale-Grey-1">Lorem ipsum dolor sit amet</span>
+                    </div>
+                </div>
+                <div class="flex flex-col gap-4">
+                    <div class="p-14 border-2 rounded-lg">
+                        <img src="@/assets/images/apply-ecomarket.svg" alt="apply-ecomarket">
+                    </div>
+                    <div class="flex flex-col text-center gap-1">
+                        <span class="text-2xl text-Black-and-White-Black font-semibold">Apply EcoMarket</span>
+                        <span class="text-sm text-Grayscale-Grey-1">Lorem ipsum dolor sit amet</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
