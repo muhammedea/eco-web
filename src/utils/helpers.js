99052e6b1a497c68d536894731a5d66a91265e81
Muhammed Emin Aydın <muhammedea@gmail.com>
2023-01-02 17:56:09 +0300

    TradeView improvements and deployment

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,9 +1,9 @@
 import { ethers } from 'ethers';
 
-export function formatAmount(input) {
+export function formatAmount(input, decimals = 2) {
   const value = ethers.utils.formatEther(input);
   const [part1, part2] = value.split('.');
-  return `${part1}.${part2.slice(0, 2)}`;
+  return `${part1}.${part2.slice(0, decimals)}`;
 }
 
 export function formatNumber(input) {
@@ -11,7 +11,11 @@ export function formatNumber(input) {
   return input.toString();
 }
 
-export function formatDate(date) {
+export function formatDate(input) {
+  let date = input;
+  if (typeof input === 'string') {
+    date = new Date(input);
+  }
   const yyyy = date.getFullYear();
   let mm = date.getMonth() + 1;
   let dd = date.getDate();
@@ -20,4 +24,27 @@ export function formatDate(date) {
   if (mm < 10) mm = `0${mm}`;
 
   return `${dd}.${mm}.${yyyy}`;
+}
+
+export function formatTime(input) {
+  let date = input;
+  if (typeof input === 'string') {
+    date = new Date(input);
+  }
+  let hh = date.getHours();
+  let mm = date.getMinutes();
+  let ss = date.getSeconds();
+
+  if (hh < 10) hh = `0${hh}`;
+  if (mm < 10) mm = `0${mm}`;
+  if (ss < 10) ss = `0${ss}`;
+
+  return `${hh}:${mm}.${ss}`;
+}
+
+export function getPercentage(partialValue, totalValue) {
+  if (ethers.BigNumber.isBigNumber(partialValue) || ethers.BigNumber.isBigNumber(totalValue)) {
+    return ethers.BigNumber.from(partialValue).mul(100).div(ethers.BigNumber.from(totalValue)).toNumber();
+  }
+  return (partialValue * 100) / totalValue;
 }
