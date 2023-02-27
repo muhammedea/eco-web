ccaaa3c89bc4d2be0fd0cefa1e2cc454867061cb
Umut KENAR <umut.kenar@hotmail.com>
2023-02-27 13:59:57 +0300

    refactor: change token modal

--- src/components/basecomponents/TradeListCard.vue
+++ src/components/basecomponents/TradeListCard.vue
@@ -4,42 +4,43 @@
     class="flex px-4 py-2 items-center justify-between w-full cursor-pointer hover:bg-Grayscale-Grey-5 hover:shadow-sm transition-all duration-200 rounded-lg"
     exact-active-class=""
   >
-    <div class="flex gap-2 md:gap-3 w-6/12">
+    <div class="flex gap-2 md:gap-3 w-7/12">
       <div class="flex">
-        <div class="w-5 h-5 md:w-10 md:h-10 rounded-full overflow-hidden">
-          <img src="@/assets/images/ampl.svg" alt="ampl" class="w-full" />
-        </div>
-        <div class="w-5 h-5 md:w-10 md:h-10 rounded-full overflow-hidden -ml-2 md:-ml-3">
-          <img src="@/assets/images/usdt.svg" alt="usdt" class="w-full" />
-        </div>
+        <span class="flex-shrink-0 w-10 h-10 rounded-full overflow-hidden border p-2">
+          <img src="@/assets/images/ampl.svg" alt="ampl" class="w-full object-contain h-full" />
+        </span>
       </div>
       <div class="flex flex-col text-Grayscale-Grey-3">
-        <div class="text-xs md:text-sm flex">
-          <span class="text-Black-and-White-Black">{{ pair.tokenA.symbol }} </span>&nbsp; / {{ pair.tokenB.symbol }}
-          <!--<span
-            class="w-[18px] h-[18px] bg-Tint-Acsend-Yellow text-Acsen-Yellow ml-2 text-xs flex justify-center items-center"
-          >
-            5X
-          </span>-->
+        <div class="flex items-center ">
+          <div class="text-xs md:text-sm flex relative">
+            <span class="text-Black-and-White-Black">{{ pair.tokenA.symbol }} </span>&nbsp; / {{ pair.tokenB.symbol }}
+            <!--<span
+              class="w-[18px] h-[18px] bg-Tint-Acsend-Yellow text-Acsen-Yellow ml-2 text-xs flex justify-center items-center"
+            >
+              5X
+            </span>-->
+            <span
+              @click="isFavorite = !isFavorite"
+              @keydown="isFavorite"
+              class="cursor-pointer hover:text-Acsen-Yellow transition-all duration-200 flex items-center justify-center absolute top-0 -right-6"
+              :class="isFavorite ? 'text-Acsen-Yellow' : 'text-[#e6e7eb]'"
+            >
+              <i class="yi yi-star-filled" style="font-size: 16px"></i>
+            </span>
+          </div>
         </div>
         <span class="text-xs leading-5">{{ pair.name }}</span>
       </div>
     </div>
-    <div class="text-xs md:text-sm text-Grayscale-Grey-3 w-3/12 flex md:flex-row flex-col justify-end items-end">
+    <div class="text-xs md:text-sm text-Grayscale-Grey-3 w-3/12 pr-1 xs:pr-0 flex md:flex-row flex-col justify-end items-end">
       <span class="text-Black-and-White-Black pr-1 md:pr-2">$ 1.01</span>
     </div>
-    <div class="flex gap-2 md:gap-6 w-3/12 justify-end items-center">
-      <!----><div class="text-xs md:text-sm text-Grayscale-Grey-3 flex md:flex-row flex-col items-center"><span class="text-Color-Code-Green pl-2">+0.5%</span></div>
+    <div class="flex gap-2 md:gap-6 w-2/12 justify-end items-center">
+      <div class="text-xs md:text-sm text-Grayscale-Grey-3 flex md:flex-row flex-col items-center">
+        <span class="text-Color-Code-Green pl-2">+0.5%</span>
+      </div>
       <!-- change > 0 :::: <div class="text-sm text-Grayscale-Grey-3">139.06K<span class="text-Color-Code-Green pl-2">+0.36%</span></div>-->
       <!-- change = 0 :::: <div class="text-sm text-Grayscale-Grey-3">139.06K<span class="pl-2">0%</span></div>-->
-      <span
-        @click="isFavorite = !isFavorite"
-        @keydown="isFavorite"
-        class="cursor-pointer hover:text-Acsen-Yellow transition-all duration-200 flex items-center justify-center"
-        :class="isFavorite ? 'text-Acsen-Yellow' : 'text-[#e6e7eb]'"
-      >
-        <i class="yi yi-star-filled" style="font-size: 16px"></i>
-      </span>
     </div>
   </router-link>
 </template>
--- src/components/tradeview/ChangeToken.vue
+++ src/components/tradeview/ChangeToken.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="px-4 pb-4 w-full min-w-[350px] md:min-w-[600px] min-h-[300px] max-h-[500px] border rounded-lg shadow-2xl bg-white z-20 overflow-y-scroll scrollbar-hide"
+    class="px-4 pb-4 w-full min-w-[350px] xs:min-w-[450px] md:min-w-[530px] min-h-[300px] max-h-[500px] border rounded-lg shadow-2xl bg-white z-20 overflow-y-scroll scrollbar-hide"
   >
     <div class="w-full h-full flex flex-col">
       <div class="flex flex-col gap-4 pt-4 sticky top-0 bg-white">
@@ -13,9 +13,9 @@
         </y-input>
         <div class="flex flex-col w-full">
           <div class="py-2 px-4 w-full">
-            <span class="text-sm text-Grayscale-Grey-3 font-mediuml w-6/12 inline-block">Pair</span>
-            <span class="text-sm text-Grayscale-Grey-3 font-medium w-3/12 inline-block text-right">Price</span>
-            <span class="text-sm text-Grayscale-Grey-3 font-medium w-3/12 inline-block text-right pr-4 md:pr-10">Change</span>
+            <span class="text-sm text-Grayscale-Grey-3 font-mediuml w-7/12 inline-block">Pair</span>
+            <span class="text-sm text-Grayscale-Grey-3 font-medium w-3/12 inline-block text-right pr-1 xs:pr-0">Price</span>
+            <span class="text-sm text-Grayscale-Grey-3 font-medium w-2/12 inline-block text-right">Change</span>
           </div>
         </div>
       </div>
